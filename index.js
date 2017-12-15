865d355541ab1a3d2d1854479520d137a8280b3f
yuta0801 <yuta20030801@icloud.com>
2017-12-15 17:52:58 +0900

    細かな修正

--- index.js
+++ index.js
@@ -1,59 +1,52 @@
-const fs = require('fs')
-const http = require('http')
-const location = 'yamaguchi-ken,jp'
-const units = 'metric'
-const APIKEY = 'APIKEY_IS_HERE'
-const URL = 'http://api.openweathermap.org/data/2.5/weather?q='+ location +'&units='+ units +'&appid='+ APIKEY
+require('dotenv').config()
 const Discord = require('discord.js')
 const client = new Discord.Client()
-const token = 'Discord_Token_here'
-const yamaguchi = JSON.parse(fs.readFileSync('./yamaguchi_weather.json', 'utf8'))
-const japanese = JSON.parse(fs.readFileSync('./japanese.json', 'utf8'))
-const ja = japanese[yamaguchi.weather[0].id]
+const request = require('request')
+const japanese = require('./japanese.json')
+const url = 'http://api.openweathermap.org/data/2.5/weather'
+const location = 'yamaguchi-ken,jp'
+const units = 'metric'
+
 client.on('ready', () => {
-  console.log('OK!')
+  console.log(`Logged in as ${client.user.tag}!`)
 })
 
 client.on('message', message => {
-  if (message.content === '!天気情報更新') {
-    http.get(URL, res => {
-      let body = ''
-      res.setEncoding('utf8')
-      res.on('data', chunk => {
-        body += chunk
-      })
-      res.on('data', () => {
-        res = JSON.parse(body)
-        fs.writeFile('yamaguchi_weather.json', JSON.stringify(res, null, '   '))
-      })
-    }).on('error', e => {
-      console.log(e.message)
+  if (message.content === '!山口県の天気') {
+    request(url, {
+      method: 'get',
+      qs: {
+        q: location,
+        units: units,
+        appid: process.env.APIKEY,
+      },
+    }, (err, res, data) => {
+      if (err) return message.reply('エラー', err.toString())
+      if (res.statusCode !== 200) return message.reply('エラー', data)
+      message.channel.send({embed: {
+        color: 3447003,
+        author: {
+          name: 'OpenWeatherMap',
+          icon_url: 'https://raw.githubusercontent.com/danyweis/pics4codepen/master/weather/icon/openweathermap.png',
+        },
+        title: '盛岡市のお天気情報',
+        url: 'https://openweathermap.org',
+        description: 'OpenWeatherMapのAPI叩いたデータです。',
+        fields: [
+          { name: '天気', value: japanese[data.weather[0].id]},
+          { name: '気温', value: data.main.temp + '°C' },
+          { name: '風力', value: data.wind.speed + 'm' },
+          { name: '風向', value: data.wind.deg + '°' },
+          { name: '雲量', value: data.clouds.all + '%' },
+        ],
+        timestamp: new Date(),
+        footer: {
+          icon_url: 'http://openweathermap.org/img/w/' + data.weather[0].icon +'.png',
+          text: 'This bot is corded by 3mdev. All rights reserved.',
+        },
+      }})
     })
   }
-  if (message.content === '!山口県の天気') {
-    message.channel.send({embed: {
-      color: 3447003,
-      author: {
-        name: 'OpenWeat herMap',
-        icon_url: 'https://raw.githubusercontent.com/danyweis/pics4codepen/master/weather/icon/openweathermap.png',
-      },
-      title: '盛岡市のお天気情報',
-      url: 'https://openweathermap.org',
-      description: 'OpenWeatherMapのAPI叩いたデータです。',
-      fields: [{
-        name: '今日の情報',
-        value: '天気 :' + ja + '\n'
-              + '気温: ' + yamaguchi.main.temp + '°C\n'
-              + '風力: ' + yamaguchi.wind.speed + 'm\n'
-              + '風向: ' + yamaguchi.wind.deg + '°\n'
-              + '雲量: ' + yamaguchi.clouds.all + '%\n',
-      }],
-      timestamp: new Date(),
-      footer: {
-        icon_url: 'http://openweathermap.org/img/w/' + yamaguchi.weather[0].icon +'.png',
-        text: 'This bot is corded by 3mdev. All rights reserved.',
-      },
-    }})
-  }
 })
-client.login(token)
+
+client.login(process.env.TOKEN)
