ae64e8b92a98e899b3632989c7d545408e5ccbeb
yuta0801 <yuta20030801@icloud.com>
2017-12-15 18:20:32 +0900

    角度から方位を表示

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const Discord = require('discord.js')
 const client = new Discord.Client()
 const request = require('request')
-const japanese = require('./japanese.json')
+const data = require('./data.json')
 const url = 'http://api.openweathermap.org/data/2.5/weather'
 const location = 'yamaguchi-ken,jp'
 const units = 'metric'
@@ -20,9 +20,9 @@ client.on('message', message => {
         units: units,
         appid: process.env.APIKEY,
       },
-    }, (err, res, data) => {
+    }, (err, res, json) => {
       if (err) return message.reply('エラー', err.toString())
-      if (res.statusCode !== 200) return message.reply('エラー', data)
+      if (res.statusCode !== 200) return message.reply('エラー', json)
       message.channel.send({embed: {
         color: 3447003,
         author: {
@@ -33,15 +33,15 @@ client.on('message', message => {
         url: 'https://openweathermap.org',
         description: 'OpenWeatherMapのAPI叩いたデータです。',
         fields: [
-          { name: '天気', value: japanese[data.weather[0].id]},
-          { name: '気温', value: data.main.temp + '°C' },
-          { name: '風力', value: data.wind.speed + 'm' },
-          { name: '風向', value: data.wind.deg + '°' },
-          { name: '雲量', value: data.clouds.all + '%' },
+          { name: '天気', value: data.weather[json.weather[0].id]},
+          { name: '気温', value: json.main.temp + '°C' },
+          { name: '風力', value: json.wind.speed + 'm' },
+          { name: '風向', value: data.direction[Math.round(json.wind.deg / 22.5)] },
+          { name: '雲量', value: json.clouds.all + '%' },
         ],
         timestamp: new Date(),
         footer: {
-          icon_url: 'http://openweathermap.org/img/w/' + data.weather[0].icon +'.png',
+          icon_url: 'http://openweathermap.org/img/w/' + json.weather[0].icon.replace('n', 'd') +'.png',
           text: 'This bot is corded by 3mdev. All rights reserved.',
         },
       }})
