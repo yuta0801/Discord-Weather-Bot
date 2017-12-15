37139f37afd580c14c06ffcc49a71cf44efdb83f
yuta0801 <yuta20030801@icloud.com>
2017-12-15 19:36:56 +0900

    好きな場所の天気の取得に対応

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const Discord = require('discord.js')
 const client = new Discord.Client()
 const request = require('request')
+const geocoder = require('geocoder')
 const data = require('./data.json')
 const url = 'http://api.openweathermap.org/data/2.5/weather'
 const location = 'yamaguchi-ken,jp'
@@ -12,40 +13,46 @@ client.on('ready', () => {
 })
 
 client.on('message', message => {
-  if (message.content === '!山口県の天気') {
-    request(url, {
-      method: 'get',
-      json: true,
-      qs: {
-        q: location,
-        units: units,
-        appid: process.env.APIKEY,
-      },
-    }, (err, res, json) => {
+  if (/^.+の天気$/.test(message.content)) {
+    const place = message.content.match(/^!(.+)の天気$/)[1]
+    geocoder.geocode(place, (err, geo) => {
       if (err) return message.reply('エラー', err.toString())
-      if (res.statusCode !== 200) return message.reply('エラー', json)
-      message.channel.send({embed: {
-        color: 3447003,
-        author: {
-          name: 'OpenWeatherMap',
-          icon_url: 'https://raw.githubusercontent.com/danyweis/pics4codepen/master/weather/icon/openweathermap.png',
+      request(url, {
+        method: 'get',
+        json: true,
+        qs: {
+          q: location,
+          units: units,
+          appid: process.env.APIKEY,
+          lat: geo.results[0].geometry.location.lat,
+          lng: geo.results[0].geometry.location.lng,
         },
-        title: '盛岡市のお天気情報',
-        url: 'https://openweathermap.org',
-        description: 'OpenWeatherMapのAPI叩いたデータです。',
-        fields: [
-          { name: '天気', value: data.weather[json.weather[0].id]},
-          { name: '気温', value: json.main.temp + '°C' },
-          { name: '風力', value: json.wind.speed + 'm' },
-          { name: '風向', value: data.direction[Math.round(json.wind.deg / 22.5)] },
-          { name: '雲量', value: json.clouds.all + '%' },
-        ],
-        timestamp: new Date(),
-        footer: {
-          icon_url: 'http://openweathermap.org/img/w/' + json.weather[0].icon.replace('n', 'd') +'.png',
-          text: 'This bot is corded by 3mdev. All rights reserved.',
-        },
-      }})
+      }, (err, res, json) => {
+        if (err) return message.reply('エラー', err.toString())
+        if (res.statusCode !== 200) return message.reply('エラー', json)
+        message.channel.send({embed: {
+          color: 3447003,
+          author: {
+            name: 'OpenWeatherMap',
+            icon_url: 'https://raw.githubusercontent.com/danyweis/pics4codepen/master/weather/icon/openweathermap.png',
+          },
+          title: place + 'のお天気情報',
+          url: 'https://openweathermap.org',
+          description: 'OpenWeatherMapのAPI叩いたデータです。',
+          fields: [
+            { name: '天気', value: data.weather[json.weather[0].id]},
+            { name: '気温', value: json.main.temp + '°C' },
+            { name: '風力', value: json.wind.speed + 'm' },
+            { name: '風向', value: data.direction[Math.round(json.wind.deg / 22.5)] },
+            { name: '雲量', value: json.clouds.all + '%' },
+          ],
+          timestamp: new Date(),
+          footer: {
+            icon_url: 'http://openweathermap.org/img/w/' + json.weather[0].icon.replace('n', 'd') +'.png',
+            text: 'This bot is corded by 3mdev. All rights reserved.',
+          },
+        }})
+      })
     })
   }
 })
